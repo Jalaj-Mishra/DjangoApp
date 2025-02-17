db130438889eb41eb53b19897e25f1cdd2fe281e
JALAJ MISHRA <mjalaj77@gmail.com>
2025-02-18 00:31:36 +0530

    Route modification

--- DjangoEmPPortal/EmpMgmt/urls.py
+++ DjangoEmPPortal/EmpMgmt/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('api/', views.home, name='home'),
     path('api/login/', views.login_view, name='login'),
     path('api/add/', views.addEmp, name='addEmp'),
-    path('empList/', views.getAllEmp, name='empList'),
-    path('get/', views.getEmpById, name='getEmpById'),   # here we are passing id in query_params
+    path('api/empList/', views.getAllEmp, name='empList'),
+    path('api/get/', views.getEmpById, name='getEmpById'),   # here we are passing id in query_params
     path('api/edit/<int:id>/', views.updateEmpById, name='updateEmp'),  # here we are passing the id in the url
     path('api/remove/<int:id>/', views.deleteEmp, name='deleteEmp'),  # here we are passing the id in the url
 ]
